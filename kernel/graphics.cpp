618a750bc3448ef7ee947d7ba6b515ad37c91344
Kohei Noda <ponserviceregist@gmail.com>
2024-02-21 11:21:32 +0900

    Add FillRectangle and DrawRectangle functions

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -17,3 +17,26 @@ void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor &c)
     p[2] = c.r;
     // p[3] is reserved byte
 }
+
+void FillRectangle(PixelWriter &writer, const Vector2D<int> &pos, const Vector2D<int> &size, const PixelColor &c)
+{
+    for (int dy = 0; dy < size.y; ++dy)
+        for (int dx = 0; dx < size.x; ++dx)
+            writer.Write(dx + pos.x, dy + pos.y, c);
+}
+
+// Draw a rectangle with a line weight of 1 pixel
+void DrawRectangle(PixelWriter &writer, const Vector2D<int> &pos, const Vector2D<int> &size, const PixelColor &c)
+{
+    for (int dx = 0; dx < size.x; ++dx)
+    {
+        writer.Write(dx + pos.x, pos.y, c);
+        writer.Write(dx + pos.x, pos.y + size.y, c);
+    }
+
+    for (int dy = 1; dy < size.y - 1; ++dy)
+    {
+        writer.Write(pos.x, dy + pos.y, c);
+        writer.Write(pos.x + size.x - 1, dy + pos.y, c);
+    }
+}
